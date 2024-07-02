1395cabba7cb3e1c573f2fbcc8dc908515af5e26
Dhruv Gupta <dhruvg-mca23@cs.du.ac.in>
2024-07-03 00:44:32 +0530

    sign in functionality with ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {Link,useNavigate} from 'react-router-dom';
 
-import jwt from 'jsonwebtoken';
+
 
 export default function SignUp() {
   const [formData,setFormData]=useState({});
