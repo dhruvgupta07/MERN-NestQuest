b73c756db4eb47ac0e282db2256a6bb95b4d1547
Dhruv Gupta <dhruvg-mca23@cs.du.ac.in>
2024-07-03 00:25:27 +0530

    created signin api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import {Link,useNavigate} from 'react-router-dom';
 
+import jwt from 'jsonwebtoken';
+
 export default function SignUp() {
   const [formData,setFormData]=useState({});
   const [error, setError] = useState(null);
