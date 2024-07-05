63fc054508dbd08596a66bf492c11b90dba8f9fa
Dhruv Gupta <dhruvg-mca23@cs.du.ac.in>
2024-07-06 02:05:53 +0530

    add create listing route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -14,22 +15,26 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-const app=express();
+
+const app = express();
+
 app.use(express.json());
+
 app.use(cookieParser());
-app.listen(3000,()=>{
-    console.log('server is running at port 3000!')
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
-app.use((err,req,res,next)=>{
-  const statusCode=err.statusCode || 500;
-  const message=err.message||"Internal Server Error!";
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
   });
